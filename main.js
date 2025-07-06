77b3cfd6792309686f3c3bcb05a6c1223722aaeb
zaidrengga <zaidrenggaa@gmail.com>
2025-07-07 01:34:36 +0700

    first comit

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll('.animate-on');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, {
+        threshold: 0.5
+    });
+
+    elements.forEach(el => observer.observe(el));
+});
